274b0b8ac04e7d0cd141295fc73552ec3f53ac7e
song426 <seolsongie@gmail.com>
2024-11-08 17:32:41 +0900

    add post endpoint

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ app.get("/", (req, res) => {
 });
 
 app.use(require("./routes/getRoutes"));
+app.use(require("./routes/deleteRoutes"));
+app.use(require("./routes/postRoutes"));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
